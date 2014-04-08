b997a64e51640163c1826dc0f791cf27a6a9fc12
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2014-04-08 17:20:26 -0400

    alert updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 TreeBook2::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+    get 'edit', to: 'devise/registrations#edit', as: :edit
+  end
+
   resources :statuses
   root to: 'statuses#index'
 
