9d7ec4bc923fa681c8bcef976cebc0dcd2a3d826
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2014-04-08 15:29:11 -0400

    Started Playing with TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :full_name, :profile_name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
+
+  validates:first_name, presence: true
+  validates:last_name, presence: true
+  validates:profile_name, presence: true, uniqueness: true,
+  format: {
+    with: /\A[a-zA-Z\-\_]+\Z/,
+    message: "Must be formatted correctly"
+  }
+
   def full_name
     "#{first_name} #{last_name}"
   end
