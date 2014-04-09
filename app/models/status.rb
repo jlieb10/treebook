e0f9f8bf95b0a4e921ab179adca18e05a5ad39f2
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2014-04-08 21:43:11 -0400

    Generated Profile Controller

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,8 @@
 class Status < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content, :user_id, :full_name, :first_name, :last_name
+
+  validates :content, presence: true, length: { minimum: 2}
+
+  validates :user_id, presence: true
 end
---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that login route opens the login page" do
+		get '/login'
+		assert_response :success
+end
